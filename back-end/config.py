9a8c36a1eb19ad534e28fab22825086d40f037b1
liujianghang <1178262698@qq.com>
2022-10-04 16:43:52 +0800

    2. Vue.js通过axios访问Flask RESTful API

--- back-end/config.py
+++ back-end/config.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 
+
+# python中使用python-dotenv这个包来读取环境变量信息
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 

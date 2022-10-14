5ca695d7e8e8087ceabc6c4994d1e544864ff99a
liujianghang <1178262698@qq.com>
2022-10-14 11:26:39 +0800

    4. Vue.js调用API实现用户注册/登录/退出

---
+++ front-end/src/components/Alert.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- 使用role属性告诉辅助设备（如屏幕阅读器）这个元素所扮演的角色，起到一个增强作用 -->
+  <!-- 这里的class是动态绑定，接收props -->
+  <div class="alert" role="alert" v-bind:class="'alert-' + variant">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Alert",
+  props:['variant','message']
+}
+</script>
+
+<style scoped>
+
+</style>
